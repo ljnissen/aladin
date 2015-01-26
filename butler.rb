b5a4cb8e938529b41dae372dac5b840b5089518a
ljnissen <lj_nissen@gmx.de>
2015-01-26 20:56:15 +0100

    Added iui-code and fixed last parts of app.

--- butler.rb
+++ butler.rb
@@ -13,7 +13,7 @@ helpers do
  
 	def file_link(file)
 		filename = Pathname.new(file).basename
-		"<a href='#{file}'>#{filename}</a><br/>"
+		"<li><a href='#{file}' target='_self'>#{filename}</a></li>"
 	end
 end 
 
@@ -24,10 +24,17 @@ __END__
 @@index
 <html>
 	<head>
+		<meta name="viewport" content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scaleable=0;"/>
+		<style type="text/css" media="screen">@import "/stylesheets/iui.css";</style>
+		<script type="application/x-javascript" src="/javascripts/iui.js"></script>
 	</head>
 
 	<body>
-		<h1>Your files, sir...</h1>
-		<%= @links %>
+		<div class="toolbar">
+			<h1 id="pageTitle"></h1>
+		</div>
+		<ul id="home" title="Your files, sir..." selected="true">
+			<%= @links %>
+		</ul>
 	</body>
 </html>
